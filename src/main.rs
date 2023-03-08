f78e93f338944c3e36d51e36d0423b679215f60a
Bene <37740907+Nereuxofficial@users.noreply.github.com>
2023-03-08 16:49:40 +0100

    Colored printing, connection loss recovery

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,6 @@
 use bytes::BytesMut;
 use clap::Parser;
+use colored::Colorize;
 use mqtt_v5::types::Packet;
 use tokio::{
     io::{AsyncReadExt, AsyncWriteExt},
@@ -22,49 +23,60 @@ async fn main() {
     } else if let Some(file) = args.file {
         let file = std::fs::read_to_string(file).unwrap();
         // Send packets from file
-        for packet in file.lines() {
-            send_packet(&mut stream, packet.to_string()).await;
+        for (nr, packet) in file.lines().enumerate() {
+            print!("{}", format!("Packet {}: ", nr + 1).yellow());
+            match send_packet(&mut stream, packet.to_string()).await{
+                Ok(_) => (),
+                Err(e) => {println!("{}", format!("Error: {:?}", e).red());
+                stream = TcpStream::connect(format!("{0}:{1}", args.host, args.port))
+                    .await
+                    .unwrap();
+                    send_packet(&mut stream, packet.to_string()).await.unwrap();
+                }
+            }
         }
     } else {
         println!("No packet or file specified");
     }
 }
 
-async fn send_packet(stream: &mut TcpStream, packet: String) {
+async fn send_packet(stream: &mut TcpStream, packet: String) -> anyhow::Result<()>{
     //Decode packet: hex -> [u8]
     let lower_hex_string = packet.to_lowercase();
     let hex_string = lower_hex_string.as_bytes();
     let decoded_buf = hex::decode(hex_string).unwrap();
-    println!("Sending: {packet:?}");
-    println!("Sending: {:?}", &decoded_buf);
+    println!("{}", format!("Sending: {:?}", &decoded_buf).green());
     if let Some(send_packet) = decode_packet(&mut decoded_buf.clone()) {
-        println!("Sending: {:?}", &send_packet);
+        println!("{}", format!("Sending: {:?}", &send_packet).green());
     } else {
-        println!("Cannot decode input packet");
+        println!("{}", "Cannot decode input packet".red());
     }
 
     //Send packet
-    stream.write_all(&decoded_buf).await.unwrap();
+    match stream.write_all(&decoded_buf).await {
+        Ok(_) => println!("{}", "Sent".green()),
+        Err(e) => return Err(anyhow::anyhow!("Error: {:?}", e)),
+    }
     stream.flush().await.unwrap();
 
     //Wait for response
-    stream.readable().await.unwrap();
+    /*
+    stream.readable().await;
 
     //Read response
     let mut buf = [0; 1024];
     let n = stream.read(&mut buf).await.unwrap();
-    println!();
 
     //Display response
     let encoded = hex::encode(&buf[..n]);
-    println!("Received: {encoded:?}");
-    println!("Received: {:?}", &buf[..n]);
+    println!("{}", format!("\nReceived: {:?}", &buf[..n]).blue());
     if let Some(packet) = decode_packet(&mut buf[..n]) {
-        println!("Received: {:?}", &packet);
+        println!("{}", format!("Received: {:?}", &packet).blue());
     } else {
-        println!("Cannot decode response packet");
-    }
-    println!("====================================");
+        println!("{}", "Cannot decode response packet".red());
+    }*/
+    println!("{}", "====================================".black());
+    Ok(())
 }
 
 /// Decode a packet from a buffer
